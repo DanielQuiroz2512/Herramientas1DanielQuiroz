661ee80673b57ef9dc101d761c86778a0876e26a
DanielQuiroz2512 <juan.quiroz168@pascualbravo.edu.co>
2025-06-03 01:55:38 -0500

    Se crean mas front

--- Proyecto/Proyecto/Fornt/Admin.cs
+++ Proyecto/Proyecto/Fornt/Admin.cs
@@ -37,5 +37,12 @@ namespace Proyecto
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Inventario1 Menu_inventario = new Inventario1();
+            Menu_inventario.Show();
+            this.Hide();
+        }
     }
 }
